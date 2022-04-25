6f026dcdf88f0ba71dc141845aee6c9b5556594e
xemmett <xemmettxlawlorx@gmail.com>
2022-04-25 12:45:46 +0100

    added joblog

--- 15-SFSB-ShoppingCart/src/java/utils/LogFile.java
+++ 15-SFSB-ShoppingCart/src/java/utils/LogFile.java
@@ -15,16 +15,38 @@ import java.io.*;
 
 public class LogFile {
     
-    public static void main(String msg) throws IOException {
+    public static void LoginLog(String msg) throws IOException {
         Logger log = Logger.getLogger(LogFile.class.getName());
 
         //
         // Create an instance of FileHandler that write log to a file called
         // app.log. Each new message will be appended at the at of the log file.
         //
-        FileHandler fileHandler = new FileHandler("login.log", true);        
-        log.addHandler(fileHandler);
-        
-        log.info(msg);
+    
+        try {
+            FileHandler fileHandler = new FileHandler("login.log", true);
+            log.addHandler(fileHandler);
+            log.info(msg);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    public static void JobLog(String msg) throws IOException {
+        Logger log = Logger.getLogger(LogFile.class.getName());
+
+        //
+        // Create an instance of FileHandler that write log to a file called
+        // app.log. Each new message will be appended at the at of the log file.
+        //
+    
+        try {
+            FileHandler fileHandler = new FileHandler("jobLog.log", true);
+            log.addHandler(fileHandler);
+            log.info(msg);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
